9290c5c7b1bfc629f429b85b4d157d6fc5c9844b
kunxin-chor <chorkunxin@yahoo.com>
2021-09-13 11:59:13 +0000

    Added basic form

---
+++ forms/index.js
@@ -0,0 +1,17 @@
+const forms = require('forms');
+
+// create some shortcuts
+const fields = forms.fields;
+const validators = forms.validators;
+
+const createProductForm = function() {
+    return forms.create({
+        // <input type="text" name="productName" etc. >
+        "name": fields.string({
+            required: true,
+            errorAfterField: true, 
+        })
+    })
+}
+
+module.exports = { createProductForm}
--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { createProductForm} = require('../forms')
 
 // import in the Product model from models/index.js
 const { Product } = require('../models');
 
 router.get('/', async function(req,res){
     // eqv. to "select * from products"
+    // analogous to: db.collection('products').find({})
     let products = await Product.collection().fetch();
     res.render('products/index',{
         'products': products.toJSON()
+    })
+})
+
+router.get('/create', function(req,res){
+    const productForm = createProductForm();
+    res.render('products/create', {
+        'form': productForm.toHTML()
     })
 })
 
